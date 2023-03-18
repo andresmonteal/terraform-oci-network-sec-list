f256a98ef8730afa9518dc4a86cbdec08d4406b1
Andres Montealegre <montealegre.af@gmail.com>
2023-03-18 17:44:00 -0500

    fix: updates versions file

--- versions.tf
+++ versions.tf
@@ -1,8 +1,12 @@
+# Copyright (c) 2022 Oracle Corporation and/or affiliates.  All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
+
 terraform {
   required_providers {
     oci = {
       source  = "hashicorp/oci"
-      version = ">=4.96.0"
+      version = ">=4.67.3"
     }
   }
+  required_version = ">= 1.0.0"
 }
